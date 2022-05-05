36b0a4d81a3d7ce36b3122759f9152c168f79f45
Leon Yao <leon.yao117@gmail.com>
2022-05-05 13:40:22 +0100

    add imports for other summit files

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,11 @@
 from tasks.summit_01 import Summit
 from tasks.summit_01_noBoxes import Summit as SummitNoBoxes
+from tasks.summit_01_noBoxes_xy import Summit as SummitNoBoxesXY
+
 
 isaacgym_task_map = {
     # "Summit": Summit,
+    # "Summit": Summit,
     "Summit": SummitNoBoxes
+    # "Summit": SummitNoBoxesXY
 }
