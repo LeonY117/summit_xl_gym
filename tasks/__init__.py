bbc6ee8848a907be1c72ead23010c89549d9a4e3
Leon Yao <leon.yao117@gmail.com>
2022-05-06 13:13:21 +0100

    add version 2

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,11 +1,12 @@
 from tasks.summit_01 import Summit
 from tasks.summit_01_noBoxes import Summit as SummitNoBoxes
 from tasks.summit_01_noBoxes_xy import Summit as SummitNoBoxesXY
+from tasks.summit_02 import Summit as Summit_02
 
 
 isaacgym_task_map = {
     # "Summit": Summit,
-    # "Summit": Summit,
-    "Summit": SummitNoBoxes
-    # "Summit": SummitNoBoxesXY
+    "Summit": SummitNoBoxes,
+    # "Summit": SummitNoBoxesXY,
+    "Summit_02": Summit_02
 }
