1f42ef41eac4a29b847d2300acf41091c5396781
Leon Yao <leon.yao117@gmail.com>
2022-05-25 16:01:36 +0100

    create version 5 with multiobjective agent

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,6 +3,8 @@ from tasks.summit_01_noBoxes import Summit as SummitNoBoxes
 from tasks.summit_01_noBoxes_xy import Summit as SummitNoBoxesXY
 from tasks.summit_02 import Summit as Summit_02
 from tasks.summit_03 import Summit as Summit_03
+from tasks.summit_04 import Summit as Summit_04
+from tasks.summit_05 import Summit as Summit_05
 
 
 isaacgym_task_map = {
@@ -10,5 +12,7 @@ isaacgym_task_map = {
     "Summit": SummitNoBoxes,
     # "Summit": SummitNoBoxesXY,
     "Summit_02": Summit_02,
-    "Summit_03": Summit_03
+    "Summit_03": Summit_03,
+    "Summit_04": Summit_04,
+    "Summit_05": Summit_05,
 }
